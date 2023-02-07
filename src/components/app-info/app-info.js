802d1f6991753e980b4c1b030bb59e98ea0a107c
Еркебулан Кундызбеков <erkebulankundyzbekov@MacBook-Air-Erkebulan.local>
2023-02-07 14:33:13 +0600

    update

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,13 +1,13 @@
-import './app-info.css'
+import "./app-info.css";
 
-const AppInfo = () => {
-    return(
+const AppInfo = ({increased, employees}) => {
+    return (
         <div className="app-info">
-            <h1>Учет сотрудников компании Жиробас Алдияр</h1>
-            <h2>Общее количество сотрудников: </h2>
-            <h2>Премию получат: </h2>
+            <h1>Учет сотрудников в компании N</h1>
+            <h2>Общее число сотрудников: {employees}</h2>
+            <h2>Премию получат: {increased}</h2>
         </div>
-    );
+    )
 }
 
 export default AppInfo;
